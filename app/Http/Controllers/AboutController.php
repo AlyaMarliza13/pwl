568723ce084ebefb7850409ba28d0ab9085d2b05
Alya Marliza Koesnanto <alyamarlizakoesnanto03@gmail.com>
2023-02-22 14:49:33 +0700

    Praktikum_2_PWL

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    function index()
+    {
+        echo "2141720004 || Alya Marliza Koesnanto";
+    }
+}
